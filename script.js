f9f099a2ab8ddd7defbc12ba2a4f68c3c589e8b9
HvK Co <92639283+HvK-Corp@users.noreply.github.com>
2023-05-30 13:31:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,39 @@
-     // Function to open the classic message
-      function openMessage() {
-        document.getElementById("gift-box").style.display = "none";
-        document.getElementById("message").style.display = "block";
-        document.getElementsByClassName("background-container")[0].style.display = "none";
-      }
-
-      // Function to open the snow globe
-      function openSnowGlobe() {
-        document.getElementById("message").style.display = "none";
-        document.getElementById("snow-globe").style.display = "block";
-        document.getElementsByClassName("background-container")[0].style.display = "block";
-        document.body.classList.add("blur"); /* Add blur effect to the body */
-        document.getElementsByClassName("background-container")[0].classList.add("blur"); /* Add blur effect to the background image */
-       }
-
-// Function to settle the snowflakes
-function settleSnowflakes() {
-  var snowflakes = document.getElementsByClassName("snow");
-
-  for (var i = 0; i < snowflakes.length; i++) {
-    var snowflake = snowflakes[i];
-    snowflake.classList.add("settled"); // Add the 'settled' class to snowflakes
-  }
+    // Function to open the classic message
+function openMessage() {
+  document.getElementById("gift-box").style.display = "none";
+  document.getElementById("message").style.display = "block";
+  document.getElementsByClassName("background-container")[0].style.display = "none";
 }
 
-// Function to animate snowflakes going up
-function animateSnowflakesUp() {
-  var snowflakes = document.getElementsByClassName("snow");
-
-  for (var i = 0; i < snowflakes.length; i++) {
-    var snowflake = snowflakes[i];
-    snowflake.classList.remove("settled"); // Remove the 'settled' class from snowflakes
-    snowflake.classList.add("active"); // Add the 'active' class to snowflakes
-  }
+// Function to open the snow globe
+function openSnowGlobe() {
+  document.getElementById("message").style.display = "none";
+  document.getElementById("snow-globe").style.display = "block";
+  document.getElementsByClassName("background-container")[0].style.display = "block";
+  document.body.classList.add("blur"); /* Add blur effect to the body */
+  document.getElementsByClassName("background-container")[0].classList.add("blur"); /* Add blur effect to the background image */
 }
 
-// Function to settle the snowflakes back after a minute
-function settleSnowflakesBack() {
-  setTimeout(function () {
-    settleSnowflakes();
-  }, 600000); // Settle the snowflakes back after 10 minutes (600,000 milliseconds)
-}
-
+// Function to start the snow animation
 function startSnowAnimation() {
-      var snowflakesContainer = document.querySelector(".snowflakes-container");
+  var snowflakesContainer = document.querySelector(".snowflakes-container");
 
-      for (var i = 0; i < 50; i++) {
-        var snowflake = document.createElement("div");
-        snowflake.classList.add("snow");
-        snowflakesContainer.appendChild(snowflake);
-      }
-    }
+  for (var i = 0; i < 50; i++) {
+    var snowflake = document.createElement("div");
+    snowflake.classList.add("snow");
+    snowflakesContainer.appendChild(snowflake);
+  }
+}
 
 // Function to shake the globe and trigger animations
 function shakeGlobe() {
-  animateSnowflakesUp(); // Animate snowflakes going up
   document.getElementById("snow-globe").classList.add("shake");
   setTimeout(function () {
     document.getElementById("snow-globe").classList.remove("shake");
-    settleSnowflakes(); // Settle the snowflakes initially
-    startSnowAnimation(); // Start the snow animation
-    settleSnowflakesBack(); // Settle the snowflakes back after a minute
+    startSnowAnimation(); // Start the snow animation immediately after the globe stops shaking
   }, 500);
-      var snowfallContainer = document.querySelector(".snowfall-container");
+
+  var snowfallContainer = document.querySelector(".snowfall-container");
   snowfallContainer.innerHTML = ""; // Clear previous snowballs if any
 
   for (var i = 1; i <= 20; i++) {
