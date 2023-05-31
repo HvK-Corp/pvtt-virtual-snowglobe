5db5002194495f54f6dd4a015ac5e247b8640393
HvK Co <92639283+HvK-Corp@users.noreply.github.com>
2023-05-31 22:21:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
- var currentDate = new Date();
-  var targetDate = new Date("July 14, 2023");
+var currentDate = new Date();
+var targetDate = new Date("July 14, 2023");
 
-  if (currentDate < targetDate) {
-   document.getElementsByClassName("not-now")[0].style.display = "block";
-    document.getElementById("gift-box").style.display = "none";
-   document.getElementsByClassName("background-container")[0].style.display = "block";
-    document.body.classList.add("blur"); /* Add blur effect to the body */
-    document.getElementsByClassName("background-container")[0].classList.add("blur"); /* Add blur effect to the background image */
-  } else {
-   document.getElementById("gift-box").style.display = "block";
-   document.getElementsByClassName("not-now")[0].style.display = "none";
-    document.getElementsByClassName("background-container")[0].style.display = "block";
-  }
+if (currentDate < targetDate) {
+  document.getElementsByClassName("not-now")[0].style.display = "block";
+  document.getElementById("gift-box").style.display = "none";
+  document.getElementsByClassName("background-container")[0].style.display = "block";
+  document.body.classList.add("blur");
+  document.getElementsByClassName("background-container")[0].classList.add("blur");
+} else {
+  document.getElementsByClassName("not-now")[0].style.display = "none";
+  document.getElementById("gift-box").style.display = "block";
+  document.getElementsByClassName("background-container")[0].style.display = "block";
+  document.body.classList.remove("blur");
+  document.getElementsByClassName("background-container")[0].classList.remove("blur");
+}
 
 // Function to open the classic message
 function openMessage() {
