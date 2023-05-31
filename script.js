0c3f353decdf626cf452c1e74a13c766c381b8e0
HvK Co <92639283+HvK-Corp@users.noreply.github.com>
2023-05-31 12:10:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ function openMessage() {
   document.getElementById("gift-box").style.display = "none";
   document.getElementById("message").style.display = "none"; // Hide the message initially
   document.getElementById("black-screen").style.display = "block";
-  document.getElementById("box.mp3").play(); // Play the box opening sound effect
+  playSound(); // Play the box opening sound effect
   setTimeout(showMessage, 9000); // Show the message after 7 seconds
+}
+
+// Function to play background music
+function playSound() {
+  var Sound = document.getElementById("tear");
+  Sound.play(); // Start playing the background music
+}
+
+// Function to pause background music
+function pauseBackgroundMusic() {
+  var Sound = document.getElementById("tear");
+  Sound.pause(); // Pause the background music
 }
 
 // Function to show the message
 function showMessage() {
   document.getElementById("message").style.display = "block"; // Show the message
+  document.getElementById("black-screen").style.display = "none";
    document.getElementsByClassName("background-container")[0].style.display = "block";
   document.body.classList.add("blur"); /* Add blur effect to the body */
   document.getElementsByClassName("background-container")[0].classList.add("blur"); /* Add blur effect to the background image */
@@ -61,16 +74,26 @@ function stopSnowAnimation() {
 
 // Function to shake the globe and trigger animations
 function shakeGlobe() {
-  document.getElementById("shake.mp3").play();
   document.getElementById("snow-globe").classList.add("shake");
   setTimeout(function () {
     document.getElementById("snow-globe").classList.remove("shake");
+    playEffect();
     startSnowfall(); // Start the snow animation immediately after the globe stops shaking
 
     // Stop the snowfall animation after approximately 30 seconds
     setTimeout(stopSnowAnimation, 30000);
   }, 500);
+// Function to play background music
+function playEffect() {
+  var effect = document.getElementById("globe");
+  effect.play(); // Start playing the background music
+}
 
+// Function to pause background music
+function pauseEffect() {
+  var effect = document.getElementById("globe");
+  effect.pause(); // Pause the background music
+}
   var snowfallContainer = document.querySelector(".snowfall-container");
   snowfallContainer.innerHTML = ""; // Clear previous snowfalls if any
 
